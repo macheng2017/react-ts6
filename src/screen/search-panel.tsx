7c359531ebe4be325686d06a10d917a2b627a8b9
mac <gos360@163.com>
2022-08-29 21:43:16 +0800

    feat:输入框获取下拉框显示内容的自己解决方案

--- src/screen/search-panel.tsx
+++ src/screen/search-panel.tsx
@@ -5,11 +5,28 @@ export const SearchPanel = () => {
         name: '',
         personId: ''
     });
+    let [users, setUsers] = useState([
+        {
+            name: '小明',
+            personId: '1'
+        },
+        {
+            name: '小花',
+            personId: '2'
+        },
+    ]);
     return <div>
         <input type="text" value={param.name} onChange={(evt) => setParam({...param, name: evt.target.value})}/>
-        <select onChange={(evt) => setParam({...param, personId: evt.target.value})}>
+        <select value={param.personId}
+                onChange={(evt) => setParam(
+                    {
+                        ...param,
+                        name: evt.target.value.split(',')[0],
+                        personId: evt.target.value.split(',')[1]
+                    })
+                }>
             <option value=''>负责人</option>
-            <option value={param.personId}>{param.name}</option>
+            {users.map(v => <option value={[v.name, v.personId]}>{v.name}</option>)}
         </select>
     </div>
 }
